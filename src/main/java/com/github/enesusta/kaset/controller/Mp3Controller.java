b5ef68c71cec16ea6f5348860340e22c2aae6663
Enes Usta <enesusta@email.com>
2020-05-24 18:33:50 +0300

    JAX-RS @QueryParam implemented

--- src/main/java/com/github/enesusta/kaset/controller/Mp3Controller.java
+++ src/main/java/com/github/enesusta/kaset/controller/Mp3Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.BufferedReader;
@@ -28,10 +29,12 @@ public class Mp3Controller {
 
     @GET
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response getStream() {
+    public Response getStream(@QueryParam("url") String url) {
+        logger.info("Url is " +url);
+        logger.info("react istek atayor.");
         return Response
                 .ok(mp3ResourceStreamingOutput)
-                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Origin", "http://localhost:3000")
                 .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
                 .header("Access-Control-Allow-Credentials", "true")
                 .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
--- src/main/js/src/components/Site.js
+++ src/main/js/src/components/Site.js
@@ -27,6 +27,31 @@ function Site() {
 
     }
 
+    const test = e => {
+
+        const apiCall = `${process.env.REACT_APP_API}/mp3?url=${url}`;
+
+        e.preventDefault();
+        fetch(apiCall)
+            .then(response => {
+                const reader = response.body.getReader();
+                console.log(reader);
+
+                reader.read().then(function processText({ done, value }) {
+                    // Result objects contain two properties:
+                    // done  - true if the stream has already given you all its data.
+                    // value - some data. Always undefined when done is true.
+                    console.log(value);
+
+                    if (done) {
+                        console.log("Stream complete");
+                        return;
+                    }
+                });
+
+            });
+    }
+
     return (
         <div className="site-container">
             <p>Insert a valid URL</p>
@@ -35,7 +60,7 @@ function Site() {
                     type="text"
                     onChange={urlHandler}
                 />
-                <button className="button" onClick={clickHandler}>Download</button>
+                <button className="button" onClick={test}>Download</button>
             </form>
         </div>
     )
